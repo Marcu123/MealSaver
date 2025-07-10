4b935dad89c305be0def2ed7a78afe3017e332a9
marcu <marcupetraru03@gmail.com>
2025-07-11 02:02:37 +0300

    sec config cors

--- MealSaver/src/main/java/com/marcu/mealsaver/Config/SecurityConfig.java
+++ MealSaver/src/main/java/com/marcu/mealsaver/Config/SecurityConfig.java
@@ -68,7 +68,7 @@ public class SecurityConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
+                        .allowedOriginPatterns("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
